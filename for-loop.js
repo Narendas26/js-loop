132c39c5b5dcb943a1addb87f98d54f7624931e5
Narendas26 <naosoft1419@gmail.com>
2026-03-20 22:00:35 +0600

    Js-loop

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// Array
+const numbers = [12, 89, 45, 63, 21, 72, 52, 81];
+
+// Print Full Array:
+console.log(numbers);
+
+// for loop
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ["Orange", "Apple", "Banana", "Jackfruit", "Watermelon"];
+
+// Basic Structure
+// for(){
+
+// }
+
+
+// Print the Array
+console.log(fruits);
+
+// Print array element using loop
+for(const fruit of fruits){
+    console.log("I Want to eat", fruit);
+}
---
+++ recap.js
@@ -0,0 +1,20 @@
+//const: value will not change
+//let : value can change
+//var : we should not use it
+
+// Variable>
+const ammount = 200;
+
+// Condition>
+if(ammount > 100){
+
+}
+else{
+
+}
+
+// array>
+const numbers = [12,32,54,65]
+
+
+
