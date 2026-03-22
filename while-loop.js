cbf830a2983e5b571cb6d62a27650d8b8d90cea0
Narendas26 <naosoft1419@gmail.com>
2026-03-22 21:19:41 +0600

    While Loop

---
+++ while-loop.js
@@ -0,0 +1,18 @@
+const number = 11;
+
+if(number < 10){
+    console.log("Choto ekta number paisi");
+}
+
+console.log("Something new is cooking");
+
+
+// While Loop:
+let num = 0;
+
+while(num < 5){
+    console.log("Step :", num);
+    //num = num + 1;
+    // num += 1;
+    num++;
+}
