1f378a24191fa1f992dbc4cd5eda5e967cb12a8f
Narendas26 <naosoft1419@gmail.com>
2026-03-22 21:53:19 +0600

    Range and sum

---
+++ range.js
@@ -0,0 +1,16 @@
+// Experiment 1
+let num = 1;
+console.log("Experiment 1");
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+
+// Experiment 2:
+let num1 = 50;
+console.log("Experiment 2");
+while(num1 <= 100){
+    console.log(num1);
+    num1++;
+}
+
---
+++ sum.js
@@ -0,0 +1,11 @@
+let num = 1;
+let sum = 0;
+
+while(num <= 10){
+    console.log(num);
+    sum += num;
+    //console.log("The Total is: ",sum);
+    num++;
+}
+
+console.log("The Total is: ",sum);
