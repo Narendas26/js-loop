8af5428c449935d3580ab1b5e21f6a59beaca180
Narendas26 <naosoft1419@gmail.com>
2026-03-23 19:31:23 +0600

    for loop & odd-even

---
+++ for.js
@@ -0,0 +1,18 @@
+// While Loop:
+console.log("While Loop:");
+let num = 0;            //Loop Variable
+while(num < 5){         //Loop Condition
+    console.log(num);   //Loop statement
+    num++;              //Loop variable value change
+}
+
+//For Loop Structure:
+// for(loop Variable declaration; Loop condition; loop variable value change){
+//     Statement; 
+// }
+
+console.log("For Loop:");
+for(let num1 = 0; num1 < 5; num1++){
+    console.log(num1);
+}
+
---
+++ odd-even.js
@@ -0,0 +1,22 @@
+console.log("Exercise 1:");
+for(let i = 1; i <= 10 ; i++){
+    console.log(i);
+}
+
+
+console.log("Exercise 2:");
+for(let i = 1; i <= 50 ; i++){
+    console.log(i);
+}
+
+
+console.log("Exercise 3:");
+for(let i = 0; i <= 20; i+=2){
+    console.log(i);
+}
+
+
+console.log("Exercise 4:");
+for(let i = 1; i <= 20; i+=2){
+    console.log(i);
+}
