391fbc92c86510b1ed9cbc64c5bf882b690017df
Narendas26 <naosoft1419@gmail.com>
2026-04-06 09:12:19 +0600

    do-while.js

---
+++ do-while.js
@@ -0,0 +1,17 @@
+// Not Very Important
+
+console.log("Using While Loop: ");
+let i = 0;
+while (i < 5){
+    console.log(i);
+    i++;
+}
+
+// do-while loop: প্রথম বারেই যদি মিথ্যা হয়ে যায় তাও অন্তত শুরুর মানটা প্রিন্ট হবে
+console.log("Using do-while loop:");
+let n = 10;
+do{
+    console.log(n);
+    n++;
+}
+while(n < 5);
